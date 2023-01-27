1a5b2acb1b47c4d20c3b493681e8eab5312abe81
Ashish-Saini47 <ashish.m.saini999@gmail.com>
2023-01-27 17:11:18 +0530

    basic Api

---
+++ API.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask import request
+import torch
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def detect():
+    reqRef = request.get_json(force=True)
+    weigth= reqRef["model"]
+    imgUrl=reqRef["imgUrl"]
+    try:
+        model = torch.hub.load('ultralytics/yolov5', 'custom', weigth )
+        results = model(imgUrl)
+
+        return jsonify({
+            "weigthURL":"Detected",
+       
+        })
+    except:
+        return jsonify({
+            "weigthURL":"not Detected",})
+
+if __name__ == '__main__':
+    app.run()
