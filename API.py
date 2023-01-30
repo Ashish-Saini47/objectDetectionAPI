5ff97bd8fdbb9b51289c22b2c743ba9f07d3accb
Ashish-Saini47 <ashish.m.saini999@gmail.com>
2023-01-30 10:03:37 +0530

    update path

--- API.py
+++ API.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify
-from flask import request
+# from flask import request
 import torch
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def detect():
-    reqRef = request.get_json(force=True)
-    weigth= reqRef["model"]
-    imgUrl=reqRef["imgUrl"]
+    # reqRef = request.get_json(force=True)
+    # weigth= reqRef["model"]
+    # imgUrl=reqRef["imgUrl"]
     try:
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path="best.tflite" ,force_reload=True)
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path="best.tflite", force_reload=True)
         results = model("1.jpeg")
-        print(results.xyxy[0] )
-        print(results.pandas().xyxy[0] )
+        # print(results.xyxy[0] )
+        # print(results.pandas().xyxy[0] )
         return jsonify({
             "weigthURL":"Detected",
        
